317d7a56437314231566b6c0eee1a3b76b92e547
jaeseongDev <qkrwotjd1445@naver.com>
2021-05-26 15:35:23 +0900

    feat : first commit

---
+++ src/test/java/com/example/restdocspractice/RestdocsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdocspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
